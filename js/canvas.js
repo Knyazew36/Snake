759f260c805c893b008ceede57d443d8ebf35671
Knyazew36 <knyazew.sv@gmail.com>
2022-12-07 22:29:40 +0300

    1

--- js/canvas.js
+++ js/canvas.js
@@ -1,10 +1,13 @@
 export default class Canvas {
   constructor(container, a, b) {
-    this.element = document.createElement("canvas");
-    this.context = this.element.getContext("2d");
-    this.element.width = a;
-    this.element.height = b;
+    // this.element = document.createElement("canvas");
+    // this.context = this.element.getContext("2d");
+    // this.element.width = a;
+    // this.element.height = b;
 
-    container.appendChild(this.element);
+    // container.appendChild(this.element);
+    this.element = document.createElement("div");
+    this.element.width = 10;
+    this.element.height = 10;
   }
 }
